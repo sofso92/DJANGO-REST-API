824085151d10b4c296198f2d6bf7a8d3f2773eda
Sofia Soriano <sofso9214@gmail.com>
2023-05-09 13:29:50 -0500

    Go model data

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -1,28 +1,13 @@
 import json
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import JsonResponse
 
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    # request --> HttpRequest -> Django 
-    # print(dir(request))
-    # request.body
-    print(request.GET)  # url query params
-    print(request.POST)  
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try: 
-        data = json.loads(body) # string of JSon data -> python dictionary
-    except:
-        pass
-    print(data)
-    # data['headers'] = request.headers # request.META -> older versions of Django
-    #print(request.headers)
-    #json.dumps(dict(request.headers))
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    return JsonResponse(data)
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['description'] = model_data.price
+    return JsonResponse(data)   
