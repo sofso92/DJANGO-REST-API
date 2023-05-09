323f1097dc376e2d705093a2c77bbd7d8c28bd4c
Sofia Soriano <sofso9214@gmail.com>
2023-05-09 16:08:24 -0500

    converted to REST framework view

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -1,12 +1,18 @@
 import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+# from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework import Response
 
 from products.models import Product
 
+@api_view
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
        data = model_to_dict(model_data), fields=['id', 'title']
-    return JsonResponse(data)   
+
+    return Response(data)
+    # return JsonResponse(data)   
