c6d6a5ddf38f53e3a5e824ce750dea89ca79bc95
Sofia Soriano <sofso9214@gmail.com>
2023-05-09 14:56:56 -0500

    method instead of manual serialization

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -1,4 +1,5 @@
 import json
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
 from products.models import Product
@@ -7,8 +8,5 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['description'] = model_data.price
+       data = model_to_dict(model_data), fields=['id', 'title']
     return JsonResponse(data)   
