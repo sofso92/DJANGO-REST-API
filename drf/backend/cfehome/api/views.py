05a9e2194f6bde598a870bbca89f88ce440b0fb2
Sofia Soriano <sofso9214@gmail.com>
2023-05-09 13:39:56 -0500

    modified views to add model data id

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -7,6 +7,7 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
+        data['id'] = model_data.id
         data['title'] = model_data.title
         data['content'] = model_data.content
         data['description'] = model_data.price
