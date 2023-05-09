13ab4048aaf1ea088122f1f46fff8a4c8d930156
Sofia Soriano <sofso9214@gmail.com>
2023-05-09 11:08:10 -0500

    modified views.py

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,5 +11,14 @@ def api_home(request, *args, **kwargs):
     # print(dir(request))
     # request.body
     body = request.body # byte string of JSON data
-    print(body)
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    data = {}
+    try: 
+        data = json.loads(body) # string of JSon data -> python dictionary
+    except:
+        pass
+    print(data)
+    # data['headers'] = request.headers # request.META -> older versions of Django
+    print(request.headers)
+    json.dumps(dict(request.headers))
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
