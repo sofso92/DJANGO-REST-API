61cb95aa560ad7fb9d6a1b561b4c26da92eec05d
Sofia Soriano <sofso9214@gmail.com>
2023-05-09 22:17:40 -0500

    experimenting w rest framework view

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -3,16 +3,16 @@ from django.forms.models import model_to_dict
 # from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
-from rest_framework import Response
+from rest_framework import response
 
 from products.models import Product
 
-@api_view
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
+    """DRF API VIEW"""
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-       data = model_to_dict(model_data), fields=['id', 'title']
-
-    return Response(data)
-    # return JsonResponse(data)   
+       data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return response(data)
+    # return JsonResponse(data)  
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -5,8 +5,9 @@ import requests
 endpoint = "http://localhost:8000/api/" # http://127.0.0.1:8000/
 
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query":"Hello World"}) # HTTP Request
-# print(get_response.text) # print raw text response
+get_response = requests.get(endpoint, json={"abc": 123}) # HTTP Request
+# print(get_response.headers)
+print(get_response.text) # print raw text response
 # print(get_response.status_code) 
 
 print(get_response.json()) # print json response
