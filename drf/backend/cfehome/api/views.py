8ee97f3e0be01b80bcb8fc99fec62b424496d3f5
Sofia Soriano <sofso9214@gmail.com>
2023-05-10 11:21:42 -0500

    fixed importing response class

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -3,7 +3,7 @@ from django.forms.models import model_to_dict
 # from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
-from rest_framework import response
+from rest_framework.response import Response
 
 from products.models import Product
 
@@ -13,6 +13,6 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-       data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return response(data)
+       data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+    return Response(data)
     # return JsonResponse(data)  
