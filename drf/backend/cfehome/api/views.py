49e1515f3910a28ffb3e08d51affe81d525f28ca
Sofia Soriano <sofso9214@gmail.com>
2023-05-12 00:29:09 -0500

    added serializer to api view

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -6,13 +6,15 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(['GET'])
 def api_home(request, *args, **kwargs):
     """DRF API VIEW"""
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-       data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
-    return Response(data)
+    if instance:
+      #  data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+      data = ProductSerializer(instance).data
+    return Response (data)
     # return JsonResponse(data)  
--- drf/backend/cfehome/products/serializers.py
+++ drf/backend/cfehome/products/serializers.py
@@ -3,11 +3,16 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields = [
             'title',
             'content',
             'price',
-            'sale_price'
+            'sale_price', # reference to property of model
+            'my_discount' # instance method
         ]
+    def get_my_discount(self, obj):
+        print(obj.id)
+        return obj.get_discount()
