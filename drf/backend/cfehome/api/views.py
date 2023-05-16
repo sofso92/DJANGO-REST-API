557eec4d6a3bd234b6fc62170efa3e58d5ab9a6c
Sofia Soriano <sofso9214@gmail.com>
2023-05-16 06:00:51 -0500

    modified back api view home and editet other view

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -1,10 +1,21 @@
-from rest_framework import generics
+import json
+from django.forms.models import model_to_dict
+# from django.http import JsonResponse
 
-from .models import Product
-from .models import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    # lookup_field = 'pk'
-  
+from products.models import Product
+from products.serializers import ProductSerializer
+
+@api_view(['POST'])
+def api_home(request, *args, **kwargs):
+    """DRF API VIEW"""
+    data = request.data
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+      #  data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+      data = ProductSerializer(instance).data
+    return Response (data)
+    # return JsonResponse(data)  
