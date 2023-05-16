2263693c85e01657bc6fa42652b4beeb098279cd
Sofia Soriano <sofso9214@gmail.com>
2023-05-16 05:54:05 -0500

    Generics -  Retrieve API view

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -1,20 +1,10 @@
-import json
-from django.forms.models import model_to_dict
-# from django.http import JsonResponse
+from rest_framework import generics
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from .models import Product
+from .models import ProductSerializer
 
-from products.models import Product
-from products.serializers import ProductSerializer
-
-@api_view(['GET'])
-def api_home(request, *args, **kwargs):
-    """DRF API VIEW"""
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-      #  data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
-      data = ProductSerializer(instance).data
-    return Response (data)
-    # return JsonResponse(data)  
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk'
+  
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 endpoint = "http://localhost:8000/api/" # http://127.0.0.1:8000/
 
 
-get_response = requests.get(endpoint, json={"abc": 123}) # HTTP Request
+get_response = requests.post(endpoint, json={"title": "Hello world"}) # HTTP Request
 # print(get_response.headers)
 # print(get_response.text) # print raw text response
 # print(get_response.status_code) 
